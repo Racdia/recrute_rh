6bfb4990ab9a910213bb5f7c048f4d58340eb00a
Racine <diallomamadouracine84@gmail.com>
2025-07-28 12:05:07 +0000

    change EC2_password on github

--- database.py
+++ database.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "postgresql://recrut_user:123456@52.89.55.119:5432/recrut"
+DATABASE_URL = "postgresql://username:password@EC2_ADDRESS:5432/recrut"
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
